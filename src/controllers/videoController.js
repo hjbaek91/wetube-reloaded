d46dab794cfd3e31ea693486e252418880a3553e
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-11-11 22:03:24 +0900

    Creating a Video with Async/Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,19 +1,20 @@
 import Video from "../models/Video";
 
-Video.find({}, (error, videos) => {});
-
 export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
   return res.render("watch", { pageTitle: `Watching` });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   return res.render("edit", { pageTitle: `Editing` });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -24,8 +25,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  // here we will add a video to the videos array.
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
   return res.redirect("/");
 };
