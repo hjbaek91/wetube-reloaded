3c4e3630bb9d1b59255657c0eb8e8c20558966c9
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-11-17 23:54:51 +0900

    Code Challenge: Edit Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -147,12 +147,29 @@ export const postEdit = async (req, res) => {
     },
     body: { name, email, username, location },
   } = req;
-  await User.findByIdAndUpdate(_id, {
-    name,
-    email,
-    username,
-    location,
-  });
-  return res.render("edit-profile");
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
 };
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect("/");
+  }
+  return res.render("change-password", { pageTitle: "Change Password" });
+};
+export const postChangePassword = (req, res) => {
+  // send notification
+  return res.redirect("/");
+};
+
 export const see = (req, res) => res.send("See User");
